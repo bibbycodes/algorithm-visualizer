21c41a578babc9aed74b4776d34e95dc384a4181
Robert <bibbycodes@gmail.com>
2020-01-27 14:49:42 +0000

    added first component

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import P5 from './components/p5'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Algorithms</h1>
+      <P5></P5>
     </div>
   );
 }
---
+++ client/src/components/p5.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class P5 extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default P5
