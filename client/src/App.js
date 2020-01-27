23ce274fe9de1aef91ab537fd80c2e14b90acf72
Robert <bibbycodes@gmail.com>
2020-01-27 22:24:01 +0000

    added canvas to front end
    Co-authored-by: natyeo@users.noreply.github.com

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import P5 from './components/p5'
+import Canvas from './components/Canvas.jsx'
 
 function App() {
   return (
     <div className="App">
       <h1>Algorithms</h1>
-      <P5></P5>
+      <Canvas></Canvas>
     </div>
   );
 }
---
+++ client/src/components/Canvas.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+class Canvas extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    this.renderArray([1,4,10,20], ctx)
+  }
+
+  renderArray = (array, ctx) => {
+    array.forEach((item, index) => {
+      ctx.fillStyle = "#FF0000";
+      ctx.fillRect(index * 30, 0, 29, item)
+    })
+
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas" width={640} height={425} />
+      </div>
+    )
+  }
+}
+
+export default Canvas
