49b37699805b6fdc70652a22ba679fe7eb156407
Robert <bibbycodes@gmail.com>
2020-01-27 14:56:56 +0000

    added p5 files

--- client/src/components/p5.jsx
+++ client/src/components/p5.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import '../p5/p5.js'
 
 class P5 extends React.Component {
   constructor(props) {
     super(props)
   }
 
+  componentDidMount() {
+  }
+
+  setup() {
+    createCanvas(640, 480);
+  }
+
+  draw() {
+    ellipse(50, 50, 80, 80);
+  }
+
   render() {
     return (
-      <div>Hello</div>
+      <div>
+        { this.setup() }
+        { this.draw() }
+        Hello
+      </div>
     )
   }
 }
