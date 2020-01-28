85b6745d1c45d95b1d0298591a3b9d6283207e58
Robert <bibbycodes@gmail.com>
2020-01-28 16:23:51 +0000

    some progress with redrawing array

--- client/src/App.js
+++ client/src/App.js
@@ -9,11 +9,30 @@ class App extends React.Component {
     this.state = {data: [4, 1, 20, 10]}
   }
 
+  updateArray = () => {
+    setTimeout(() => {
+      this.setState({ data: [5,4,30,1] })
+      console.log("update array", this.state.data)
+    }, 1000)
+  }
+
+  componentDidMount() {
+    this.updateArray()
+  }
+
+  drawCanvas = () => {
+    console.log("inside draw canvas", this.state.data)
+    return (
+      <Canvas data={this.state.data} />
+    )
+  }
+
   render() {
+    console.log("rendering")
     return (
       <div className="App">
         <h1>Algorithms</h1>
-        <Canvas data={this.state.data} />
+        {this.drawCanvas()}
       </div>
     );
   }
--- client/src/components/Canvas.jsx
+++ client/src/components/Canvas.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 class Canvas extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {data : this.props.data}
   }
 
   componentDidMount() {
     const canvas = this.refs.canvas
     const ctx = canvas.getContext("2d")
+    this.setState({
+      data : this.props.data,
+      ctx : ctx
+    })
     this.renderArray(this.props.data, ctx)
   }
 
@@ -16,6 +21,12 @@ class Canvas extends React.Component {
       ctx.fillStyle = "#FF0000";
       ctx.fillRect(index * 30, 0, 29, item)
     })
+  }
+
+  componentDidUpdate() {
+    console.log("Canvas.componentDidUpdate state: ", this.state)
+    console.log("Canvas.componentDidUpdate props: ", this.props)
+    this.renderArray(this.props.data, this.state.ctx)
   }
 
   render() {
